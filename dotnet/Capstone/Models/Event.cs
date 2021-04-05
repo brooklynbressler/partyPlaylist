647dfc6b9eb3a6604c88f64bdf2a6a13bdc06c69
Brian Davis <78177585+thebrianjdavis@users.noreply.github.com>
2021-04-05 14:45:17 -0400

    vs db models init

---
+++ dotnet/Capstone/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Capstone.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public int DjUserId { get; set; }
+        public int HostUserId { get; set; }
+        public int PlaylistId { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ dotnet/Capstone/Models/Song.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Capstone.Models
+{
+    public class Song
+    {
+        public int SongId { get; set; }
+        public string SongName { get; set; }
+        public string ArtistName { get; set; }
+    }
+}
