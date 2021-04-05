48f09a0c5f018dbb2037cdf62349c40cab7710b6
brooklyn bressler <brooklynbressler@gmail.com>
2021-04-05 14:18:36 -0400

    creating db

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -20,6 +20,40 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	salt varchar(200) NOT NULL,
-	user_role varchar(50) NOT NULL
+	user_role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
-)
+);
+
+CREATE TABLE events (
+	event_id int IDENTITY(1,1) NOT NULL,
+	dj_user_id int NOT NULL,
+	host_user_id int NOT NULL,
+	playlist_id int NOT NULL,
+	event_name varchar(50) NOT NULL,
+	event_date date,
+	start_time time,
+	end_time time,
+	CONSTRAINT PK_event PRIMARY KEY (event_id),
+	CONSTRAINT FK_event_dj FOREIGN KEY (dj_user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE playlists (
+	playlist_id int IDENTITY(1,1) NOT NULL,
+	CONSTRAINT PK_playlist PRIMARY KEY (playlist_id)
+);
+
+CREATE TABLE songs (
+	song_id int IDENTITY(1,1) NOT NULL,
+	song_name varchar(255) NOT NULL,
+	artist_name varchar(255) NOT NULL,
+	CONSTRAINT PK_song PRIMARY KEY (song_id)
+);
+
+CREATE TABLE playlist_songs (
+	playlist_id int NOT NULL,
+	song_id int NOT NULL,
+	CONSTRAINT PK_playlist_song PRIMARY KEY (playlist_id, song_id),
+	CONSTRAINT FK_playlist_id FOREIGN KEY (playlist_id) REFERENCES playlists(playlist_id),
+	CONSTRAINT FK_song_id FOREIGN KEY (song_id) REFERENCES songs(song_id)
+);
+
