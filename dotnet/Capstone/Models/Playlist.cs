273e15cc89f0b41aeac5ed4aa3e761405522e24b
David Morgan <dwmorgan24@gmail.com>
2021-04-08 14:27:44 -0400

    added playlist model to backend

---
+++ dotnet/Capstone/Models/Playlist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Playlist
+    {
+        // PlaylistId  playlist_id
+        // SongId  song_id
+        // SongScore  song_score
+        // SongName  song_name
+        // ArtistName  artist_name
+        // Genre  genre
+        public int PlaylistId { get; set; }
+        public int SongId { get; set; }
+        public int SongScore { get; set; }
+        public string SongName { get; set; }
+        public string ArtistName { get; set; }
+        public string Genre { get; set; }
+
+
+    }
+}
