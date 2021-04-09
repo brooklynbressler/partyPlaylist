e66945f95af4e0df3126c712b538765639c8a95e
zak murphy <zechariahwmurphy@gmail.com>
2021-04-09 13:43:21 -0400

    modified song table to display dynamically

--- vue/src/components/SongVotingTable.vue
+++ vue/src/components/SongVotingTable.vue
@@ -1,30 +1,34 @@
 <template>
-  <div>
+  <div id="main-container">
     <table>
       <thead>
         <tr>
-          <th>Album Cover</th>
-          <th>Song and Artist</th>
-          <th>Vote Button</th>
+          <th>Party Playlist</th>
+          <th>Song and Artis</th>
+          <th>Vote Buttoms</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td><img src={{}} alt="" v-bind="possiblePlaylist.albumCover"></td>
-          <td>some song - some artist</td>
+        <tr v-for="song in possiblePlaylist" v-bind:key="song.songId">
           <td>
-            <v-btn id="likebtn" class="mx-2" icon outlined fab dark color="green darken-3" v-model="totalVotes" v-on:click="toggleUpvote">
+            <v-avatar rounded size="60">
+              <img v-bind:src="song.albumCover" alt="">
+            </v-avatar>
+          </td>
+          <td id="song-artist-info">{{song.songName}} - {{song.artistName}}</td>
+          <td>
+            <v-btn small id="likebtn" class="mx-2" icon outlined fab dark color="green darken-3" v-model="totalVotes" v-on:click="toggleUpvote">
               <v-icon dark> mdi-thumb-up </v-icon>
             </v-btn>
 
-            <v-btn id="dislikebtn" class="mx-2" icon outlined fab dark color="red darken-2" v-model="totalVotes" v-on:click="toggleDownvote">
+            <v-btn small id="dislikebtn" class="mx-2" icon outlined fab dark color="red darken-2" v-model="totalVotes" v-on:click="toggleDownvote">
               <v-icon dark> mdi-thumb-down </v-icon>
             </v-btn>
           </td>            
         </tr>
       </tbody>
     </table>
-  </div>
+  </div>  
 </template>
 
 <script>
@@ -61,6 +65,36 @@ export default {
             albumCover: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/BrandNewMan.jpg/220px-BrandNewMan.jpg',
             songName: 'Neon Moon',
             artistName: 'Brooks & Dunn',          
+            },
+            {
+            songId: 6,
+            albumCover: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/BrandNewMan.jpg/220px-BrandNewMan.jpg',
+            songName: 'Neon Moon',
+            artistName: 'Brooks & Dunn',          
+            },
+            {
+            songId: 7,
+            albumCover: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/BrandNewMan.jpg/220px-BrandNewMan.jpg',
+            songName: 'Neon Moon',
+            artistName: 'Brooks & Dunn',          
+            },
+            {
+            songId: 8,
+            albumCover: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/BrandNewMan.jpg/220px-BrandNewMan.jpg',
+            songName: 'Neon Moon',
+            artistName: 'Brooks & Dunn',          
+            },
+            {
+            songId: 9,
+            albumCover: 'https://images-na.ssl-images-amazon.com/images/I/51uC0yguxEL.jpg',
+            songName: 'Friends in Low Places',
+            artistName: 'Garth Brooks',          
+            },
+            {
+            songId: 10,
+            albumCover: 'https://images-na.ssl-images-amazon.com/images/I/51uC0yguxEL.jpg',
+            songName: 'Friends in Low Places',
+            artistName: 'Garth Brooks',          
             }
         ]
     }
@@ -69,4 +103,12 @@ export default {
 </script>
 
 <style>
+thead {
+  display: none;
+}
+
+#song-artist-info {
+  padding-left: 20px;
+  padding-right: 8px
+}
 </style>
